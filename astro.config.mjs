8c273e238f92032584f911c5f6595f4ab6ee4fa3
DrKariJaask <kari@lionrep.com>
2026-08-03 17:38:21 +0300

    Fix site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://howtowriteyourmemoir.coms",
+  site: "https://howtowriteyourmemoirs.com",
   integrations: [sitemap()],
 });
